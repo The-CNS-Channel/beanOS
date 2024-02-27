73eb68401be98096a97997212e0ea101e870a8e8
Chicken Nuggets Studios <146603883+The-CNS-Channel@users.noreply.github.com>
2024-02-26 20:31:31 -0600

    Create exit.c

---
+++ gnu-efi/lib/exit.c
@@ -0,0 +1,18 @@
+#include "lib.h"
+
+VOID
+Exit(
+    IN EFI_STATUS   ExitStatus,
+    IN UINTN        ExitDataSize,
+    IN CHAR16       *ExitData OPTIONAL
+    )
+{
+    uefi_call_wrapper(BS->Exit,
+            4,
+            LibImageHandle,
+            ExitStatus,
+            ExitDataSize,
+            ExitData);
+
+    for (;;) { }
+}
